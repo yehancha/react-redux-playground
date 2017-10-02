f5dfb3805fe846152fd37b4de7a8718b7fa49a15
Yehan Pemarathne <yehancha@gmail.com>
2017-10-02 16:46:59 +0530

    Add tests for add todo

--- src/test/ToggleTodo.js
+++ src/test/ToggleTodo.js
@@ -27,5 +27,46 @@ const testToggleTodo = () => {
   ).toEqual(todoAfter);
 }
 
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ]
+    default:
+      return state;
+  }
+}
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+}
+
+
 testToggleTodo();
+testAddTodo();
 console.log('Tests passed!');
