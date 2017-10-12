72bb6bc0d31ea64b6fe02d1b3fb80923cae8fef3
Yehan Pemarathne <yehancha@gmail.com>
2017-10-12 16:41:17 +0530

    Seperate TodoList and Todo components

--- src/index.js
+++ src/index.js
@@ -75,6 +75,33 @@ const FilterLink = ({ filter, currentFilter, children }) => {
   };
 }
 
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li onClick={onClick}
+    style={{
+      textDecoration: completed ? 'line-through' : 'none'
+    }}>
+    {text}
+  </li>
+)
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo => (
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}/>
+    ))}
+  </ul>
+)
+
 const getVisibleTodos = (todos, filter) => {
   switch (filter) {
     case 'SHOW_ALL':
@@ -109,22 +136,12 @@ class TodoApp extends Component {
         }}>
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo =>
-            <li key={todo.id}
-              onClick={() => {
-                store.dispatch({
-                  type: 'TOGGLE_TODO',
-                  id: todo.id
-                })
-              }}
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none'
-              }}>
-              {todo.text}
-            </li>
-          )}
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id => store.dispatch({
+            type: 'TOGGLE_TODO',
+            id
+          })}/>
         <p>
           Show:{' '}
           <FilterLink filter='SHOW_ALL' currentFilter={visibilityFilter}>All</FilterLink>{' '}
