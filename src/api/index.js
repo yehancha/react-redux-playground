bf71bcfbf614971f4704252222835ad96c7daab9
Yehan Pemarathne <yehancha@gmail.com>
2017-10-18 00:10:52 +0530

    Integrate addTodo api endpoint lesson#25

--- src/api/index.js
+++ src/api/index.js
@@ -25,9 +25,6 @@ const delay = (ms) =>
 
 export const fetchTodos = (filter) =>
   delay(1000).then(() => {
-    if (Math.random() > 0.5)
-      throw new Error('Boom!');
-
     switch (filter) {
       case 'all':
         return fakeDatabase.todos;
@@ -38,4 +35,22 @@ export const fetchTodos = (filter) =>
       default:
         throw new Error(`Unknown filter: ${filter}`);
     }
+  });
+
+export const addTodo = (text) =>
+  delay(1000).then(() => {
+    const todo = {
+      id: v4(),
+      text,
+      completed: false
+    };
+    fakeDatabase.todos.push(todo);
+    return todo;
+  });
+
+export const toggleTodo = (id) =>
+  delay(1000).then(() => {
+    const todo = fakeDatabase.todos.find(t => t.id === id);
+    todo.completed = !todo.completed;
+    return todo;
   });
--- src/reducers/byId.js
+++ src/reducers/byId.js
@@ -7,6 +7,11 @@ const byId = (state = {}, action) => {
         nextState[todo.id] = todo;
       });
       return nextState;
+    case 'ADD_TODO_SUCCESS':
+      return {
+        ...state,
+        [action.response.id]: action.response
+      };
     default:
       return state;
   }
